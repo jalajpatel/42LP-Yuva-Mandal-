ec5a545343bd546f1a2697f0c82db2e787875c6e
jalajpatel <96616944+jalajpatel@users.noreply.github.com>
2024-06-01 12:08:38 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className="relative flex w-full items-center justify-between px-5 py-4 bg-black text-white md:px-10">
+    <div className="relative flex w-full items-center justify-between px-5 py-4 bg-black bg-opacity-70 text-white md:px-10">
       <Head>
         <title>Jalaj P Patel</title>
       </Head>
@@ -40,7 +40,7 @@ const Header: React.FC = () => {
           {isMenuOpen ? <IoMdClose className="text-2xl" /> : <IoMdMenu className="text-2xl" />}
         </button>
       </div>
-      <ul className={`fixed top-0 left-0 z-50 h-full w-full flex flex-col items-center justify-center bg-black text-lg transition-transform transform ${isMenuOpen ? "translate-y-0" : "-translate-y-full"} md:relative md:top-auto md:left-auto md:h-auto md:w-auto md:flex-row md:bg-transparent md:text-xs md:translate-y-0`}>
+      <ul className={`fixed top-0 left-0 z-50 h-full w-full flex flex-col items-center justify-center bg-black bg-opacity-70 text-lg transition-transform transform ${isMenuOpen ? "translate-y-0" : "-translate-y-full"} md:relative md:top-auto md:left-auto md:h-auto md:w-auto md:flex-row md:bg-transparent md:text-xs md:translate-y-0`}>
         <div className="absolute top-4 right-4 md:hidden">
           <button onClick={toggleMenu}>
             <IoMdClose className="text-2xl" />
@@ -60,11 +60,14 @@ const Header: React.FC = () => {
             </Link>
           </motion.li>
         ))}
-        <div className="mt-5 md:mt-0 md:ml-6">
-          <Link href="/login" passHref>
-            <IoMdPerson className="text-lg cursor-pointer" />
-          </Link>
-        </div>
+<div className="mt-5 md:mt-0 md:ml-6">
+  <Link href="/login" passHref
+    className="flex items-center gap-2"> {/* Add flex container and gap */}
+      <IoMdPerson className="text-lg cursor-pointer" />
+      LogIn
+  </Link>
+</div>
+
       </ul>
     </div>
   );
